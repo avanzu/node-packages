0b8eb3a19d414726666a67deb5efff8e19701955
Marc Bach <mail@avanzu.de>
2022-04-08 18:15:09 +0200

    refactor: simplifies error factories

--- packages/rhea-composable/lib/errors.js
+++ packages/rhea-composable/lib/errors.js
@@ -2,33 +2,32 @@ const panic = (error) => {
     throw error
 }
 
+const releasable = (target, props) =>
+    Object.assign(target, { props, conclude: (delivery) => delivery.release(props) })
+
+const rejectable = (target, props) =>
+    Object.assign(target, { props, conclude: (delivery) => delivery.reject(props) })
+
 const undeliverable = (description) =>
-    Object.assign(new Error(description), {
-        props: { undeliverable_here: true },
-        conclude: (delivery) =>
-            delivery.release({
-                undeliverable_here: true,
-            }),
+    releasable(new Error(description), {
+        undeliverable_here: true,
     })
 
 const processFault = (reason) =>
-    Object.assign(new Error(`${reason}`), {
-        props: { condition: 'processing:failed', description: `${reason}` },
-        conclude: (delivery) =>
-            delivery.reject({
-                condition: 'processing:failed',
-                description: `${reason}`,
-            }),
+    rejectable(new Error(`${reason}`), {
+        condition: 'processing:failed',
+        description: `${reason}`,
     })
 
 const noSuchRequest = (key) =>
-    Object.assign(new Error(`Request [${key}] not found`), {
-        props: { condition: 'request:notfound', description: `Request [${key}] not found` },
-        conclude: (delivery) =>
-            delivery.reject({
-                condition: 'request:notfound',
-                description: `Request [${key}] not found`,
-            }),
+    releasable(new Error(`Request [${key}] not found`), {
+        delivery_failed: true,
     })
 
-module.exports = { panic, undeliverable, processFault, noSuchRequest }
+const requestTimedOut = (key) =>
+    releasable(new Error(`Request [${key}] timed out`), {
+        condition: 'request:timeout',
+        description: `Request [${key}] timed out`,
+    })
+
+module.exports = { panic, undeliverable, processFault, noSuchRequest, requestTimedOut }
