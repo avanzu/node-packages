e91e8b836006dddfecb834eb34bc46bbdb56d926
Marc Bach <mail@avanzu.de>
2022-10-01 19:09:59 +0200

    chore: reintegrates authentication local for benchmarking

--- services/ostium/src/authentication.js
+++ services/ostium/src/authentication.js
@@ -1,11 +1,14 @@
 const { AuthenticationService, JWTStrategy } = require('@feathersjs/authentication')
+const { LocalStrategy } = require('@feathersjs/authentication-local/lib')
 // const { expressOauth } = require('@feathersjs/authentication-oauth')
 const { LocalThreadedStrategy } = require('./authentication/strategies/local')
+
 module.exports = (app) => {
     const authentication = new AuthenticationService(app)
 
     authentication.register('jwt', new JWTStrategy())
     authentication.register('local', new LocalThreadedStrategy())
+    // authentication.register('local', new LocalStrategy())
 
     app.use('/authentication', authentication)
     // app.configure(expressOauth())
