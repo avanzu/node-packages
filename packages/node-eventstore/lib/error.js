06df600c0eb72a010ef772b46124b55e43360e96
Marc Bach <mail@avanzu.de>
2022-10-09 10:55:59 +0200

    refactor: accumulates error messages in error module

--- packages/node-eventstore/lib/error.js
+++ packages/node-eventstore/lib/error.js
@@ -8,4 +8,19 @@ class StoreError extends Error {
     }
 }
 
-module.exports = StoreError
+const Messages = {
+    NO_STORE: 'eventstore not injected!',
+    NO_STREAM: 'eventstream not injected!',
+    NO_EVENT: 'event not injected!',
+    NO_QUERY: 'query not injected!',
+    NO_AGGREGATEID: 'missing property "aggregateId"',
+    NO_UNCOMMITTED: 'eventstream.uncommittedEvents not injected!',
+    COMMIT_UNCALLABLE: 'eventstore.commit not injected!',
+    EVENTS_NO_ARRAY: 'events should be an array!',
+    EVENTS_MISSING_REV: 'The events passed should all have a streamRevision!',
+}
+
+module.exports = {
+    StoreError,
+    Messages,
+}
