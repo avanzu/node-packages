ab9469c18003a336292258a9de61ef7ad8751521
Marc Bach <mail@avanzu.de>
2022-10-09 10:56:20 +0200

    chore: adds more utility functions

--- packages/node-eventstore/lib/util.js
+++ packages/node-eventstore/lib/util.js
@@ -1,24 +1,28 @@
 const { Ok, fromPredicate } = require('@avanzu/std').Result
+const { Messages } = require('./error')
 // prettier-ignore
-const { always, has, is, identity, filter, isNil, complement, anyPass, isEmpty } = require('ramda')
+const { always, has, is, identity, filter, isNil, complement, anyPass, isEmpty, equals } = require('ramda')
 
 const noop = identity
 const isString = is(String)
+const isNoString = complement(isString)
+const isFunction = is(Function)
+const isNoFunction = complement(isFunction)
+const isCallable = (x) => equals('function', typeof x)
+const isArray = is(Array)
+const isNoArray = complement(isArray)
+
 const isNothing = anyPass([isNil, isEmpty])
-const filterEmpty = filter(complement(isNil))
+const isNotNil = complement(isNil)
+const filterEmpty = filter(isNotNil)
 const noopAsync = () => Promise.resolve()
 
-const toQuery = (aggregateId) => ({ aggregateId })
-
-const normalizeQuery = (query) =>
-    fromPredicate(isString, query).bimap(always(query), toQuery).unwrapWith(Ok)
-
-const requireId = (query) =>
-    fromPredicate(has('aggregateId'), query).mapErr(
-        () => new Error('An aggregateId should be passed!')
-    )
-
-const toAggregateQuery = (query) => normalizeQuery(query).unwrapWith(requireId)
+const isAggregate = has('aggregateId')
+const withAggregate = (aggregateId) => ({ aggregateId })
+const normalizeQuery = (q) =>
+    fromPredicate(isString, q).bimap(always(q), withAggregate).unwrapWith(Ok)
+const requireId = (obj) => fromPredicate(isAggregate, obj).mapErr(() => Messages.NO_AGGREGATEID)
+const toAggregateQuery = (q) => normalizeQuery(q).unwrapWith(requireId)
 
 module.exports = {
     noop,
@@ -28,4 +32,12 @@ module.exports = {
     requireId,
     filterEmpty,
     isNothing,
+    isNotNil,
+    isFunction,
+    isArray,
+    isString,
+    isNoString,
+    isNoFunction,
+    isNoArray,
+    isCallable,
 }
