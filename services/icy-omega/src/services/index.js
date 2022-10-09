2dc72bc89c330f9edf5ec33978d7b97b74b3e6d6
Marc Bach <mail@avanzu.de>
2022-10-09 11:05:19 +0200

    chore: autoformat

--- services/icy-omega/src/services/index.js
+++ services/icy-omega/src/services/index.js
@@ -1,7 +1,7 @@
-const dummy = require('./dummy/dummy.service.js');
-const parallel = require('./parallel/parallel.service.js');
+const dummy = require('./dummy/dummy.service.js')
+const parallel = require('./parallel/parallel.service.js')
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
-  app.configure(dummy);
-  app.configure(parallel);
+    app.configure(dummy)
+    app.configure(parallel)
 }
