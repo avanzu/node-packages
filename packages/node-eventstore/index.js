5489894728310686f7719fbca94ba036d336e7ac
Marc Bach <mail@avanzu.de>
2022-10-09 11:02:09 +0200

    refactor: restructures store factory to use Option and Result

--- packages/node-eventstore/index.js
+++ packages/node-eventstore/index.js
@@ -1,82 +1,59 @@
-var Eventstore = require('./lib/eventstore'),
+const { always, curry } = require('ramda')
+const Eventstore = require('./lib/eventstore'),
     Base = require('./lib/base'),
-    _ = require('lodash'),
-    debug = require('debug')('@avanzu/eventstore')
+    // _ = require('lodash'),
+    fs = require('fs'),
+    path = require('path'),
+    { isCallable, isString } = require('./lib/util'),
+    { Option, Result } = require('@avanzu/std')
 
-function exists(toCheck) {
-    var _exists = require('fs').existsSync || require('path').existsSync
-    if (require('fs').accessSync) {
-        _exists = function (toCheck) {
-            try {
-                require('fs').accessSync(toCheck)
-                return true
-            } catch (e) {
-                return false
-            }
-        }
+const fileExists = Option.fromNullable(fs.accessSync)
+    .map((f) => (s) => Result.tryNow(f, s).isOk())
+    .or(Option.fromNullable(fs.existsSync))
+    .or(Option.fromNullable(path.existsSync))
+    .fold(Result.Err, Result.Ok)
+
+const confirmFileExists = curry((type, toCheck) =>
+    fileExists
+        .ap(Result.of(toCheck))
+        .chain(Result.fromBoolean)
+        .bimap(
+            always(`Expected implementation for "${type}" in "${toCheck}" which does not exist!`),
+            always(toCheck)
+        )
+)
+
+const tryToRequire = (path) => Result.tryNow(require, path)
+
+const warnOnModuleNotFound = curry((type, { message, code }) => {
+    if (code === 'MODULE_NOT_FOUND') {
+        const moduleName = message.match(/'([^']+)/).at(1)
+        console.warn(
+            `Please install module "${moduleName}" to work with db implementation "${type}"!`
+        )
     }
-    return _exists(toCheck)
+})
+
+const requireStore = (type) =>
+    Result.Ok(path.join(__dirname, '/lib/databases/', `${type}.js`))
+        .chain(confirmFileExists(type))
+        .chain(tryToRequire)
+        .tapErr(warnOnModuleNotFound(type))
+        .unwrap()
+
+const getSpecificStore = (options = {}) => {
+    const type = Option.fromNullable(options.type)
+        .map((v) => (isString(v) ? v.toLowerCase() : v))
+        .unwrapOr('inmemory')
+
+    return Result.fromBoolean(isCallable(type))
+        .map(always(type))
+        .unwrapOrElse(() => requireStore(type))
 }
 
-function getSpecificStore(options) {
-    options = options || {}
-
-    options.type = options.type || 'inmemory'
-
-    if (_.isFunction(options.type)) {
-        return options.type
-    }
-
-    options.type = options.type.toLowerCase()
-
-    var dbPath = __dirname + '/lib/databases/' + options.type + '.js'
-
-    if (!exists(dbPath)) {
-        var errMsg = 'Implementation for db "' + options.type + '" does not exist!'
-        console.log(errMsg)
-        debug(errMsg)
-        throw new Error(errMsg)
-    }
-
-    try {
-        var db = require(dbPath)
-        return db
-    } catch (err) {
-        if (
-            err.message.indexOf('Cannot find module') >= 0 &&
-            err.message.indexOf("'") > 0 &&
-            err.message.lastIndexOf("'") !== err.message.indexOf("'")
-        ) {
-            var moduleName = err.message.substring(
-                err.message.indexOf("'") + 1,
-                err.message.lastIndexOf("'")
-            )
-            var msg =
-                'Please install module "' +
-                moduleName +
-                '" to work with db implementation "' +
-                options.type +
-                '"!'
-            console.log(msg)
-            debug(msg)
-        }
-
-        throw err
-    }
-}
-
-module.exports = function (options) {
-    options = options || {}
-
-    var Store
-
-    try {
-        Store = getSpecificStore(options)
-    } catch (err) {
-        throw err
-    }
-
-    return new Eventstore(options, new Store(options))
-}
+module.exports = (options = {}) =>
+    Result.tryNow(getSpecificStore, options)
+        .map((Store) => new Eventstore(options, new Store(options)))
+        .unwrap()
 
 module.exports.Store = Base
