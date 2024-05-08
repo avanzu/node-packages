9eff4e101b199fed9f1486c6161eef951bbde2a6
Marc Bach <mail@avanzu.de>
2024-05-08 17:56:04 +0200

    chore: adds typed overload for container.register method

--- packages/kernel/src/interfaces/container.ts
+++ packages/kernel/src/interfaces/container.ts
@@ -1,3 +1,7 @@
-import { AwilixContainer } from "awilix";
+import { AwilixContainer, NameAndRegistrationPair, Resolver } from "awilix";
 
-export interface Container<Services extends {} = {}> extends AwilixContainer<Services> {}
+export interface Container<Services extends {} = {}> extends AwilixContainer<Services> {
+    register<K extends keyof Services>(name: K, value: Resolver<Services[K]>) : this
+    register(name: string | symbol, value: Resolver<unknown>): this
+    register(registration: NameAndRegistrationPair<Services>) : this
+}
