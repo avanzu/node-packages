e5b8f9c28017a5eadfc3fc5a657d4b97a86fbc1b
Marc Bach <mail@avanzu.de>
2024-05-13 23:34:29 +0200

    fix: rectifies incorrect env var mapping

--- services/foundation/config/default.ts
+++ services/foundation/config/default.ts
@@ -2,7 +2,9 @@ import { deferConfig } from 'config/defer'
 import { type Options as AJVOpts } from 'ajv'
 import type { AuthenticatorOptions, ResourceMap } from '@avanzu/kernel'
 import type { Resources } from '../src/application/interfaces'
+import type { Algorithm } from 'jsonwebtoken'
 import mikroOrm from './mikro-orm'
+
 export default {
     host: deferConfig(() => process.env.HOST || 'localhost'),
     port: deferConfig(() => process.env.PORT || 9090),
@@ -11,37 +13,39 @@ export default {
         level: 'debug',
     })),
     redis: deferConfig(() => ({
-        connectionName: process.env.REDIS_HOST || 'foundation-cache',
-        port: 6379,
-        host: '127.0.0.1',
-        username: 'default',
-        password: '',
-        db: 0,
+        connectionName: process.env.REDIS_CONNECTION_NAME || 'foundation-cache',
+        port: process.env.REDIS_PORT || 6379,
+        host: process.env.REDIS_HOST,
+        username: process.env.REDIS_USER || 'default',
+        password: process.env.REDIS_PASSWD || '',
+        db: process.env.REDIS_DB || 0,
         lazyConnect: true,
     })),
-    validation: deferConfig(() : AJVOpts  => ({
-        allErrors: true,
-        strict: false,
-        useDefaults: true,
-        allowUnionTypes: true
-    })),
-    authentication: deferConfig(() : AuthenticatorOptions => ({
-        secret: process.env.JWT_SECRET || 'ya3mdsDb4jHvTymEV9rfWQG5zhJVNheZ',
-        jwt: {
-            algorithm: 'HS512',
-            issuer: process.env.JWT_ISSUER || 'avanzu_auth',
-            expiresIn: process.env.JWT_EXPIY || '1d',
-        }
-    })),
+    validation: deferConfig(
+        (): AJVOpts => ({
+            allErrors: true,
+            strict: false,
+            useDefaults: true,
+            allowUnionTypes: true,
+        })
+    ),
+    authentication: deferConfig(
+        (): AuthenticatorOptions => ({
+            secret: process.env.JWT_SECRET || 'ya3mdsDb4jHvTymEV9rfWQG5zhJVNheZ',
+            jwt: {
+                algorithm: (process.env.JWT_ALGO || 'HS512') as Algorithm,
+                issuer: process.env.JWT_ISSUER || 'avanzu_auth',
+                expiresIn: process.env.JWT_EXPIY || '1d',
+            },
+        })
+    ),
     orm: deferConfig(() => mikroOrm),
-    resources: deferConfig(() : Resources => ({
-        authentication: {
-            url: '',
-            auth: true
-        },
-        configuration: {
-            duration: 5,
-            prefix: 'foo'
-        }
-    }))
+    resources: deferConfig(
+        (): Resources => ({
+            authentication: {
+                url: '',
+                auth: true,
+            },
+        })
+    ),
 }
